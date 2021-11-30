d1e9abcebc3b2b1c7c3eda4f3e93e04d73b4bef6
mich101mich <mich101mich@gmail.com>
2021-11-30 15:22:03 +0100

    prep init

---
+++ src/days/day_01.rs
@@ -0,0 +1,22 @@
+use crate::utils::*;
+
+#[allow(unused)]
+pub fn run() {
+    #[allow(unused_variables)]
+    let input = include_str!("../input/01.txt");
+    // let input = ;
+    
+    let parsed = input
+        //.lines()
+        //.chars()
+        //.map(|l| l.chars().to_vec())
+        //.map(parse)
+        //.map(|l| scanf!(l, "{}", isize).unwrap())
+        //.to_vec()
+        //.sum::<isize>()
+        //.parse::<isize>()
+        ;
+    
+    //pv!(parsed);
+    
+}
