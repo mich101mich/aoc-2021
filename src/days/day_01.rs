91dcef43ceeac217f48c2679f886631806658225
mich101mich <mich101mich@gmail.com>
2021-12-01 08:16:10 +0100

    day 01

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -4,19 +4,29 @@ use crate::utils::*;
 pub fn run() {
     #[allow(unused_variables)]
     let input = include_str!("../input/01.txt");
-    // let input = ;
-    
-    let parsed = input
-        //.lines()
-        //.chars()
-        //.map(|l| l.chars().to_vec())
-        //.map(parse)
-        //.map(|l| scanf!(l, "{}", isize).unwrap())
-        //.to_vec()
-        //.sum::<isize>()
-        //.parse::<isize>()
-        ;
-    
-    //pv!(parsed);
-    
+
+    let parsed = input.lines().map(parse).to_vec();
+    let sums = parsed.windows(3).map(|w| w.iter().sum::<isize>()).to_vec();
+    let increased = sums
+        .iter()
+        .zip(sums.iter().skip(1))
+        .filter(|(a, b)| b > a)
+        .count();
+
+    pv!(increased);
+}
+
+#[allow(unused)]
+pub fn part_one() {
+    #[allow(unused_variables)]
+    let input = include_str!("../input/01.txt");
+
+    let parsed = input.lines().map(parse).to_vec();
+    let increased = parsed
+        .iter()
+        .zip(parsed.iter().skip(1))
+        .filter(|(a, b)| b > a)
+        .count();
+
+    pv!(increased);
 }
