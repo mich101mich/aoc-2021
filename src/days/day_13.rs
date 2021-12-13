e5fa3a1a628e9cbbf98b5e51072f35213d9e52ab
mich101mich <mich101mich@gmail.com>
2021-12-13 16:39:40 +0100

    optimized for larger inputs

--- src/days/day_13.rs
+++ src/days/day_13.rs
@@ -6,44 +6,47 @@ pub fn run() {
     let input = include_str!("../input/13.txt");
 
     let mut iter = input.lines();
-    let points = iter
+    let mut points = iter
         .by_ref()
         .take_while(|s| !s.is_empty())
         .map(|l| scanf!(l, "{},{}", usize, usize).unwrap())
-        .to_vec();
+        .to_set();
 
+    for (axis, pos) in iter.filter_map(|s| scanf!(s, "fold along {}={}", char, usize)) {
+        let mut new_points = HashSet::with_capacity(points.len());
+        if axis == 'x' {
+            for p in &points {
+                if p.0 < pos {
+                    new_points.insert(*p);
+                } else {
+                    new_points.insert((2 * pos - p.0, p.1));
+                }
+            }
+        } else {
+            for p in &points {
+                if p.1 < pos {
+                    new_points.insert(*p);
+                } else {
+                    new_points.insert((p.0, 2 * pos - p.1));
+                }
+            }
+        }
+        points = new_points;
+    }
     let (mut w, mut h) = points
         .iter()
         .fold((0, 0), |(w, h), (x, y)| (w.max(*x + 1), h.max(*y + 1)));
 
-    let mut grid = Grid::new_clone((w, h), false);
-    for p in points {
-        grid[p] = true;
-    }
-
-    for (axis, pos) in iter.filter_map(|s| scanf!(s, "fold along {}={}", char, usize)) {
-        if axis == 'x' {
-            for y in 0..h {
-                for x in 0..pos {
-                    let v = grid[(w - 1 - x, y)];
-                    let target = &mut grid[(x, y)];
-                    *target = *target || v;
-                }
+    for y in 0..h {
+        for x in 0..w {
+            if points.contains(&(x, y)) {
+                print!("#");
+            } else {
+                print!(" ");
             }
-            w = pos;
-        } else {
-            for x in 0..w {
-                for y in 0..pos {
-                    let v = grid[(x, h - 1 - y)];
-                    let target = &mut grid[(x, y)];
-                    *target = *target || v;
-                }
-            }
-            h = pos;
         }
-        grid.trim_to((w, h));
+        println!();
     }
-    grid.print('#', ' ');
 }
 
 #[allow(unused)]
@@ -67,7 +70,10 @@ pub fn part_one() {
         grid[p] = true;
     }
 
-    for (axis, pos) in iter.take(1).filter_map(|s| scanf!(s, "fold along {}={}", char, usize)) {
+    for (axis, pos) in iter
+        .take(1)
+        .filter_map(|s| scanf!(s, "fold along {}={}", char, usize))
+    {
         if axis == 'x' {
             for y in 0..h {
                 for x in 0..pos {
